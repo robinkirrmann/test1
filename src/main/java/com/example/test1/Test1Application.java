bf52bf5d674cc91989987b87e78484169b4b4103
Robin Kirrmann <robin.kirrmann@studmail.htw-aalen.de>
2022-10-17 16:09:34 +0200

    commit test

--- src/main/java/com/example/test1/Test1Application.java
+++ src/main/java/com/example/test1/Test1Application.java
@@ -2,12 +2,22 @@ package com.example.test1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class Test1Application {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(Test1Application.class, args);
+		int ergebnis = 3+4;
+		System.out.println(ergebnis);
 	}
+
+
+
+
+
 
 }
